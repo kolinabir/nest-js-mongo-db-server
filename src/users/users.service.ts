6d61330faf45e668fad361a05d8c9100112e705e
kolinabir <knkolin9@gmail.com>
2024-07-15 23:37:22 +0600

    chore: Add initial project files and configuration

---
+++ src/users/users.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/schemas/User.schema';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectModel(User.name)
+    ){}
+}
